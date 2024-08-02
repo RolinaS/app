31315c047f71e44bdd909cc779c4c2db49654d9b
RolinaS <sami.rolina@outlook.fr>
2024-08-02 17:01:00 +0200

    ajout des recette dans le client

---
+++ client/src/views/repas/Dejeuner.js
@@ -0,0 +1,59 @@
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import { Table } from 'react-bootstrap';
+
+function Dejeuner() {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    // Fonction pour récupérer les données de l'API
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:3000/api/repas/Déjeuner');
+        setData(response.data);
+      } catch (error) {
+        console.error('Erreur lors de la récupération des données', error);
+        setError('Erreur lors de la récupération des données.');
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Déjeuner</h1>
+        <Table striped bordered hover responsive>
+          <thead>
+            <tr>
+              <th>Nom</th>
+              <th>Description</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.length > 0 ? (
+              data.map((item, index) => (
+                <tr key={index}>
+                  <td>{item.nom}</td>
+                  <td>{item.description}</td> {/* Assurez-vous que les noms des propriétés correspondent */}
+                </tr>
+              ))
+            ) : (
+              <tr>
+                <td colSpan="5">Aucune donnée disponible</td>
+              </tr>
+            )}
+          </tbody>
+        </Table>
+      </header>
+    </div>
+  );
+}
+
+export default Dejeuner;
